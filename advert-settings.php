ce1beb19863cd7f267f87e215be57d2568391cb0
Bhaskar K <xlinkerz@gmail.com>
2021-01-20 13:01:50 +1100

    lint fix

--- advert-settings.php
+++ advert-settings.php
@@ -161,35 +161,35 @@ class AdvertisementSettings {
 		ob_start();
 		wp_nonce_field( 'nonce_action_advert_meta_box', 'advert_meta_box_nonce' );
 		?>
-	<div class="advert-metabox">
-	<div id="field-1">
-	<input type="checkbox" name="<?php echo self::ATTR_ADVERT_ENABLED; ?>"
-		<?php echo checked( $advert_enabled, 'On' ); ?>>
-	<label for="enable-advert">Advertisements</label>
-	</div>
-	<div id="field-2">
-	<input type="radio" id="contentChoice1" name="<?php echo self::ATTR_ADVERT_TYPE; ?>"
-	value="none" <?php echo checked( $advert_type, 'none' ); ?>>
-	<label for="contentChoice1">None</label>
+			<div class="advert-metabox">
+				<div id="field-1">
+					<input type="checkbox" name="<?php echo self::ATTR_ADVERT_ENABLED; ?>"
+						<?php echo checked( $advert_enabled, 'On' ); ?>>
+					<label for="enable-advert">Advertisements</label>
+				</div>
+				<div id="field-2">
+					<input type="radio" id="contentChoice1" name="<?php echo self::ATTR_ADVERT_TYPE; ?>"
+					value="none" <?php echo checked( $advert_type, 'none' ); ?>>
+					<label for="contentChoice1">None</label>
 
-	<input type="radio" id="contentChoice2" name="<?php echo self::ATTR_ADVERT_TYPE; ?>"
-	value="sponsored-content" <?php echo checked( $advert_type, 'sponsored-content' ); ?>>
-	<label for="contentChoice2">Sponsored Content</label>
+					<input type="radio" id="contentChoice2" name="<?php echo self::ATTR_ADVERT_TYPE; ?>"
+					value="sponsored-content" <?php echo checked( $advert_type, 'sponsored-content' ); ?>>
+					<label for="contentChoice2">Sponsored Content</label>
 
-	<input type="radio" id="contentChoice3" name="<?php echo self::ATTR_ADVERT_TYPE; ?>"
-	name="advert-type" <?php echo checked( $advert_type, 'partnered-content' ); ?>>
-	<label for="contentChoice3">Partnered Content</label>
+					<input type="radio" id="contentChoice3" name="<?php echo self::ATTR_ADVERT_TYPE; ?>"
+					name="advert-type" <?php echo checked( $advert_type, 'partnered-content' ); ?>>
+					<label for="contentChoice3">Partnered Content</label>
 
-	<input type="radio" id="contentChoice4" name="<?php echo self::ATTR_ADVERT_TYPE; ?>"
-	value="brought-to-you-by" <?php echo checked( $advert_type, 'brought-to-you-by' ); ?>>
-	<label for="contentChoice4">Brought to you by</label>
-	</div>
-	<div id="field-3">
-	<label for="advert-name">Advertiser Name</label>
-	<input type="text" id="advert-name" name="<?php echo self::ATTR_ADVERT_TYPE; ?>"
-	value="<?php echo $advertiser_name; ?>">
-	</div>
-	</div>
+					<input type="radio" id="contentChoice4" name="<?php echo self::ATTR_ADVERT_TYPE; ?>"
+					value="brought-to-you-by" <?php echo checked( $advert_type, 'brought-to-you-by' ); ?>>
+					<label for="contentChoice4">Brought to you by</label>
+				</div>
+				<div id="field-3">
+					<label for="advert-name">Advertiser Name</label>
+					<input type="text" id="advert-name" name="<?php echo self::ATTR_ADVERT_TYPE; ?>"
+					value="<?php echo $advertiser_name; ?>">
+				</div>
+			</div>
 		<?php
 		echo ob_get_clean();
 	}
@@ -215,61 +215,6 @@ class AdvertisementSettings {
 		update_post_meta( $post_id, $_POST['enable-advert'], true );
 		update_post_meta( $post_id, $_POST['advert-type'], true );
 		update_post_meta( $post_id, sanitize_text_field( $_POST['advertiser-name'] ), true );
-	}
-
-	/**
-	 * Registers all block assets so that they can be enqueued through the block editor
-	 * in the corresponding context.
-	 *
-	 * @see https://developer.wordpress.org/block-editor/tutorials/block-tutorial/applying-styles-with-stylesheets/
-	 * @throws Error Build Error message.
-	 */
-	function gutenberg_block() {
-		$dir = __DIR__;
-
-		$script_asset_path = "$dir/build/index.asset.php";
-		if ( ! file_exists( $script_asset_path ) ) {
-			throw new Error(
-				'You need to run `npm start` or `npm run build` for the "create-block/advert-settings" block first.'
-			);
-		}
-
-		$index_js     = 'build/index.js';
-		$script_asset = require( $script_asset_path );
-
-		wp_register_script(
-			'create-block-advert-settings-block-editor',
-			plugins_url( $index_js, __FILE__ ),
-			$script_asset['dependencies'],
-			$script_asset['version']
-		);
-
-		wp_set_script_translations( 'create-block-advert-settings-block-editor', 'advert-settings' );
-
-		$editor_css = 'build/index.css';
-		wp_register_style(
-			'create-block-advert-settings-block-editor',
-			plugins_url( $editor_css, __FILE__ ),
-			[],
-			filemtime( "$dir/$editor_css" )
-		);
-
-		$style_css = 'build/style-index.css';
-		wp_register_style(
-			'create-block-advert-settings-block',
-			plugins_url( $style_css, __FILE__ ),
-			[],
-			filemtime( "$dir/$style_css" )
-		);
-
-		register_block_type(
-			'create-block/advert-settings',
-			[
-				'editor_script' => 'create-block-advert-settings-block-editor',
-				'editor_style'  => 'create-block-advert-settings-block-editor',
-				'style'         => 'create-block-advert-settings-block',
-			]
-		);
 	}
 }
 
